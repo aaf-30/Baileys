21ee744241eb50503559f6ae1cfac93076c73e1e
aaf-30 <81393721+aaf-30@users.noreply.github.com>
2024-03-17 02:51:21 +0700

    Update messages-recv.ts

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -681,6 +681,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 			// "missing message from node" fix	
 			logger.debug(node, 'missing body; sending ack then ignoring.')
 			await sendRetryRequest(node)
+			await sendMessageAck(node)
 			console.log("Permintaan percobaan ulang dikirim", node.attrs)
 			return
 		}
